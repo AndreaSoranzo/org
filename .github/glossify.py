2b0e32f1ebc628167324ed5f6fd8e39b6c68cce5
Andrea Soranzo <andreajsm03@gmail.com>
2025-03-12 15:48:35 +0100

    sdf

--- .github/glossify.py
+++ .github/glossify.py
@@ -9,7 +9,7 @@ G_KEYWORD = r"\\textsubscript{g}"
 SKIP_LATEX_TAGS = [r"\\section",r"\\subsection",r"\\subsubsection",r"\\paragraph",r"\\includegraphics",r"\\url",r"\\includepdf",r"\\nameref"]
 
 def main(UseThread:bool=False):
-    glox_path = path.Path(DOCS_PATH+"/Generali/glossario/01_content.tex")
+    glox_path = path.Path(DOCS_PATH+"PB/Generali/glossario/01_content.tex")
     
     defs = ReadAllWords(glox_path)
 
